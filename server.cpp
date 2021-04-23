652fe5d008e3a0ece2bf9f954919428326468f42
yao_z1 <zhenzhong.yao@joynext.com>
2021-04-23 03:02:23 -0700

    fix server re_accept from client

--- server.cpp
+++ server.cpp
@@ -17,8 +17,8 @@ int main(){
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+    serv_addr.sin_addr.s_addr = inet_addr("10.70.9.60");  //具体的IP地址
+    serv_addr.sin_port = htons(12345);  //端口
     bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
     //进入监听状态，等待用户发起请求
@@ -33,24 +33,32 @@ int main(){
     char str[] = "http://c.biancheng.net/socket/";
     write(clnt_sock, str, sizeof(str));
 
-    char buffer[40];
+   char buffer[40];
     string ChaoSu_str ="1";
     string FrontCrash_str = "2";
 
+    printf("line:%d ",__LINE__); 
     while(1)
     {
     memset(buffer,0,40);
-    read(clnt_sock, buffer, sizeof(buffer)-1);
+    
+    if( read(clnt_sock, buffer, sizeof(buffer)-1) <=0)
+     {	
+         close(clnt_sock);
+         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size); 
+         continue;
+      }
     string buf_str = buffer;
-  
-    if(buf_str.compare(ChaoSu_str) ==0 )
-        printf("Line: %d,Chao Su yujing:-->HMI; -->tts\n",__LINE__); 
+   if(buf_str.compare(ChaoSu_str) ==0 )
+        printf("Chao Su yujing\n-->HMI; -->tts\n"); 
    else if(buf_str.compare(FrontCrash_str) ==0 )
-        printf("Line: %d,FrontCrash:-->HMI;-->tts\n",__LINE__);
+        printf("FrontCrash\n--HMI;--tts\n");
    else
-       { printf("cound not compare any Message form server: %s\n", buffer); break;}
+       { printf("cound not compare any Message form server: %s\n", buffer); continue;}
+     
+    printf("line:%d ",__LINE__); 
     }
-   
+
     //关闭套接字
     close(clnt_sock);
     close(serv_sock);
